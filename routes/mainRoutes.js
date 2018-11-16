e680093f58c276182940838292aca1bfc4905348
Aaron Rosenberg <aaronrbg@gmail.com>
2018-11-16 01:08:41 +0000

    added route TODO and helper function TODO comments

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -13,24 +13,26 @@ router.get("/", (req, res) => {
   
   //user goes to menu
   router.get("/smoothies/", (req, res) => {
-    //grab smoothies data and store as variable
+    //grab smoothies data and pass to anon callback
     smoothieHelpers.getSmoothies( (err, result) => {
-
+      //on result, store smoothie data in template vars as smoothies
       const templateVars = {
         smoothies: result
       }
-
       console.log(templateVars);
+      //renter smoothies and pass in template vars
       res.render("smoothies", templateVars)
-      
     })
-    
   });
   
   //user goes to shopping cart
   router.get("/orders/new/", (req, res) => {
     //cart form is populated from cookie
-    res.render("cart");
+    const cart = 'cookie'//grab cart data from cookie
+    const templateVars = {
+      cart
+    }
+    res.render("cart", templateVars);
   });
   
   //user submits thier order
@@ -38,11 +40,6 @@ router.get("/", (req, res) => {
     //sends order to db
     //grabs order id
     //redirects to /orders/:id
-  });
-  
-  //user sees their order page and status
-  router.get("/orders/:id/", (req, res) => {
-    res.render("order");
   });
   
   //user cancels an order
