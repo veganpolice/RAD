83103c3780ec1d5521c94389b37a0ac2822adb54
Rachel Wong <rwrachelwong@gmail.com>
2018-11-16 22:18:32 +0000

    Add smoothie object from DB to menu page

--- routes/mainRoutes.js
+++ routes/mainRoutes.js
@@ -10,21 +10,21 @@ module.exports = (smoothieHelpers) => {
   router.get("/", (req, res) => {
       res.render("index");
     });
-  
+
   //user goes to menu
   router.get("/smoothies/", (req, res) => {
     //grab smoothies data and pass to anon callback
     smoothieHelpers.getSmoothies( (err, result) => {
       //on result, store smoothie data in template vars as smoothies
       const templateVars = {
-        smoothies: result
+        smoothies: result,
       }
       console.log(templateVars);
       //renter smoothies and pass in template vars
       res.render("smoothies", templateVars)
     })
   });
-  
+
   //user goes to shopping cart
   router.get("/orders/new/", (req, res) => {
     //cart form is populated from cookie
@@ -34,14 +34,18 @@ module.exports = (smoothieHelpers) => {
     }
     res.render("cart", templateVars);
   });
-  
-  //user submits thier order
+
+  router.get("/orders/:id/", (req, res) => {
+    res.render("order");
+  });
+
+  //user submits their order
   router.post("/orders/", (req, res) => {
     //sends order to db
     //grabs order id
     //redirects to /orders/:id
   });
-  
+
   //user cancels an order
   router.delete("/orders/:id/", (req, res) => {
     //sets order status to canceled in db
