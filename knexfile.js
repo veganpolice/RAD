2e0eca3367b8506de1fa4dabe16bc457b7e1506e
aaronrbg <aaronrbg@aaronrbg.com>
2018-11-17 12:42:09 -0800

    testing knex settings

--- knexfile.js
+++ knexfile.js
@@ -22,12 +22,8 @@ module.exports = {
   },
 
   production: {
-    client: 'postgresql',
+    client: 'pg',
     connection: process.env.DATABASE_URL,
-    pool: {
-      min: 2,
-      max: 10
-    },
     migrations: {
       directory: './db/migrations',
       tableName: 'migrations'
