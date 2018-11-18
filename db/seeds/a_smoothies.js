0096e057f3867dd4d618a1dc3b7ed2fe23845a9a
David Lacho <david.lacho@gmail.com>
2018-11-18 14:18:44 -0800

    seeds out

--- db/seeds/a_smoothies.js
+++ db/seeds/a_smoothies.js
@@ -1,6 +1,6 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
-  return knex.('smoothies').del()
+  return knex('smoothies').del()
     .then(function() {
       return Promise.all([
         // Inserts seed entries
