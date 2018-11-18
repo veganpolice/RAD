6e3ffcbc2256875ca8f77da71e19009f337682ea
David Lacho <david.lacho@gmail.com>
2018-11-18 14:11:35 -0800

    seeds out

--- db/seeds/e_cookies.js
+++ db/seeds/e_cookies.js
@@ -1,24 +1,24 @@
 exports.seed = function(knex, Promise) {
   // Deletes ALL existing entries
   return knex('cookies').del()
-    .then(function() {
-      return Promise.all([
-        // Inserts seed entries
-        knex('cookies').insert({
-          order_id: 1,
-          cookie: {
-            '1': 1,
-            '1': 1,
-            '1': 1,
-          },
-        }),
-        knex('cookies').insert({
-          order_id: 2,
-          cookie: {
-            '1': 4,
-            '4': 2,
-          },
-        }),
-      ]);
-    });
+    // .then(function() {
+    //   return Promise.all([
+    //     // Inserts seed entries
+    //     knex('cookies').insert({
+    //       order_id: 1,
+    //       cookie: {
+    //         '1': 1,
+    //         '1': 1,
+    //         '1': 1,
+    //       },
+    //     }),
+    //     knex('cookies').insert({
+    //       order_id: 2,
+    //       cookie: {
+    //         '1': 4,
+    //         '4': 2,
+    //       },
+    //     }),
+    //   ]);
+    // });
 };
