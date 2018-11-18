b1059d4f89542cd22b8dfea029bd78e92b0d6f7e
David Lacho <david.lacho@gmail.com>
2018-11-18 14:05:11 -0800

    added logging out errors

--- db/seeds/d_order_items.js
+++ db/seeds/d_order_items.js
@@ -9,48 +9,48 @@ exports.seed = function(knex, Promise) {
         }),
         knex('order_items').insert({
           id: 2,
-          smoothie_id: 2,
+          smoothie_id: 1,
           order_id: 1
         }),
         knex('order_items').insert({
           id: 3,
-          smoothie_id: 3,
+          smoothie_id: 1,
           order_id: 1
         }),
         knex('order_items').insert({
           id: 4,
           smoothie_id: 1,
-          order_id: 2
+          order_id: 1
         }),
         knex('order_items').insert({
           id: 5,
-          smoothie_id: 4,
-          order_id: 2
+          smoothie_id: 1,
+          order_id: 1
         }),
         knex('order_items').insert({
           id: 6,
-          smoothie_id: 4,
-          order_id: 2
+          smoothie_id: 1,
+          order_id: 1
         }),
         knex('order_items').insert({
           id: 7,
-          smoothie_id: 8,
-          order_id: 3
+          smoothie_id: 1,
+          order_id: 1
         }),
         knex('order_items').insert({
           id: 8,
-          smoothie_id: 7,
-          order_id: 3
+          smoothie_id: 1,
+          order_id: 1
         }),
         knex('order_items').insert({
           id: 9,
-          smoothie_id: 6,
-          order_id: 3
+          smoothie_id: 1,
+          order_id: 1
         }),
         knex('order_items').insert({
           id: 10,
-          smoothie_id: 5,
-          order_id: 3
+          smoothie_id: 1,
+          order_id: 1
         }),
       ]);
     });
