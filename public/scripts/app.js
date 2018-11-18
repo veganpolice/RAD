2e8acf7fd575cbebc2422b09f5d22eb30d645a67
aaronrbg <aaronrbg@aaronrbg.com>
2018-11-17 17:33:26 -0800

    changes nav to static with style fixs. adds animations for adding to car

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -88,6 +88,71 @@ $(() => {
   }
   calculateTotalPrice();
 
+
+  /*
+	Add to cart fly effect with jQuery. - May 05, 2013
+	(c) 2013 @ElmahdiMahmoud - fikra-masri.by
+	license: https://www.opensource.org/licenses/mit-license.php
+  */ 
+  $('.add-to-cart').on('click', function () {
+    const cart = $('.shopping-cart');
+    console.log('!!!', cart);
+    const imgtodrag = $(event.target).parent().parent().find("img").eq(0);
+    console.log('!!!!!!', imgtodrag);
+    if (imgtodrag) {
+        var imgclone = imgtodrag.clone()
+            .offset({
+            top: imgtodrag.offset().top,
+            left: imgtodrag.offset().left
+        })
+            .css({
+            // 'opacity': '0.5',
+                'position': 'absolute',
+                // 'height': '25%',
+                // 'width': '25%',
+                'z-index': '100'
+        })
+            .appendTo($('body'))
+            .animate({
+            'top': cart.offset().top + 10,
+            'left': cart.offset().left + 10,
+                'width': 25,
+                'height': 50
+        }, 1000, 'easeOutExpo');
+
+        function shake(div) {
+          var interval = 100;
+          var distance = 10;
+          var times = 3;
+  
+          $(div).css('position', 'relative');
+  
+          for (var iter = 0; iter < (times + 1) ; iter++) {
+              $(div).animate({
+                  top: ((iter % 2 == 0 ? distance : distance * -1))
+              }, interval);
+          }                                                                                                          
+          $(div).animate({ top: 0 }, interval);
+      }
+        
+        setTimeout(function () {
+            console.log(cart)
+            shake(cart);
+            
+            // effect("shake", {
+            //     times: 2
+            // }, 200);
+        }, 1000);
+
+        imgclone.animate({
+            'width': 0,
+                'height': 0
+        }, function () {
+            $(this).detach()
+        });
+    }
+});
+
 });
 
 //on cart submit
