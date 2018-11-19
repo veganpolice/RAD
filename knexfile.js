d2e141398e703f5532943ff8b604679d26d3f284
aaronrbg <aaronrbg@aaronrbg.com>
2018-11-18 18:22:36 -0800

    commented out that pesky line that brreaks heroky

--- knexfile.js
+++ knexfile.js
@@ -1,5 +1,5 @@
 // This is commented out when pushing to heroku. If there's an error on your local machine try uncommenting this out.
-require('dotenv').config();
+// require('dotenv').config();
 
 module.exports = {
 
