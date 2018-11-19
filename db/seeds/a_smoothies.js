e38a892a564f4d00427904974b6d98c0cc4952ab
aaronrbg <aaronrbg@aaronrbg.com>
2018-11-18 22:17:24 -0800

    updates seeds with lighter images

--- db/seeds/a_smoothies.js
+++ db/seeds/a_smoothies.js
@@ -8,42 +8,42 @@ exports.seed = function(knex, Promise) {
           // id: 1,
           description: 'Berry, Berry Good',
           price_cents: 998,
-          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/blueberry-small.png',
+          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/blueberry2.png',
           default_time: '300000'
         }),
         knex.into('smoothies').insert({
           // id: 3,
           description: 'RAD Rainbow',
           price_cents: 2531,
-          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/rainbow.png',
+          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/rainbow2.png',
           default_time: '300000'
         }),
         knex.into('smoothies').insert({
           // id: 2,
           description: 'Cash Money Mix',
           price_cents: 898,
-          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/green-small.png',
+          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/green2.png',
           default_time: '300000'
         }),
         knex.into('smoothies').insert({
           // id: 3,
           description: 'Pink Flamingo',
           price_cents: 798,
-          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/strawberry-small.png',
+          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/strawberry2.png',
           default_time: '300000'
         }),
         knex.into('smoothies').insert({
           // id: 3,
           description: 'MerMajik',
           price_cents: 1349,
-          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/blue.png',
+          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/blue2.png',
           default_time: '300000'
         }),
         knex.into('smoothies').insert({
           // id: 3,
           description: 'CoCoNutty',
           price_cents: 949,
-          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/yellow.png',
+          picture_url: 'https://s3.ca-central-1.amazonaws.com/rad-lighthouselabs/yellow2.png',
           default_time: '300000'
         })
       ]);
